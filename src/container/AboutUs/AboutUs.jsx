15b439a6815f4ea8923eda527c76356adc33f50c
Aizhan Alekbarova <alekbarova.aizhana@gmail.com>
2022-02-11 22:06:23 +0100

    header and about us section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
-
-import './AboutUs.css';
+import React from "react";
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
+    <div
+        className="app__aboutUs app__bg flex__center section__padding "
+        id="about"
+    >
+        <div className="app__aboutUs-overlay flex__center">
+            <img src={images.G} alt="letter_G" />
+        </div>
+        <div className="app__aboutUs-content flex__center">
+            <div className="app__aboutUs-content_about">
+                <h1 className="headtext__cormorant">About Us</h1>
+                <img src={images.spoon} alt="spoon" />
+                <p className="p__opensans">
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium, totam rem
+                    aperiam, eaque ipsa quae ab illo inventore veritatis et
+                    quasi architecto beatae vitae dicta sunt explicabo.
+                </p>
+                <button type="button" className="custom__button">
+                    Know more
+                </button>
+            </div>
+        </div>
+        <div className="app__aboutUs-content_knife flex__center">
+            <img src={images.knife} alt="knife" />
+        </div>
+        <div className="app__aboutUs-content_history">
+            <h1 className="headtext__cormorant">Our History</h1>
+            <img src={images.spoon} alt="spoon" />
+            <p className="p__opensans">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo.
+            </p>
+            <button type="button" className="custom__button">
+                Know more
+            </button>
+        </div>
+    </div>
 );
 
 export default AboutUs;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-
-import './Header.css';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+    <div className="app__header app__wrapper section__padding" id="home">
+        <div className="app__wrapper_info">
+            <SubHeading title="Chase the New Flavor" />
+            <h1 className="app__header-h1">The Key to Fine Dining</h1>
+            <p
+                className="p__opensans"
+                style={{ margin: "2rem 0", color: "#fff" }}
+            >
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+                voluptas sit aspernatur aut odit aut fugit, sed quia
+                consequuntur magni dolores eos qui ratione voluptatem sequi
+                nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+                sit amet, consectetur, adipisci velit, sed.
+            </p>
+        </div>
+        <div className="app__wrapper_img">
+            <img src={images.welcome} alt="header-img" />
+        </div>
+    </div>
 );
 
 export default Header;
