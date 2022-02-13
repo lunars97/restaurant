60ac15b3a91baed9e64f3396a90d02d1bdc98cd2
Aizhan Alekbarova <alekbarova.aizhana@gmail.com>
2022-02-13 16:55:18 +0100

    chef's section is added

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ const App = () => (
         <Header />
         <AboutUs />
         <SpecialMenu />
-        {/* <Chef />
+        <Chef />
         <Intro />
         <Laurels />
         <Gallery />
         <FindUs />
-        <Footer /> */}
+        <Footer />
     </div>
 );
 
